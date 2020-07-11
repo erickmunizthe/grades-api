d661eb86ff3aba087a65a0ce8014b7f22a4a03e4
erickmunizthe <34745038+erickmunizthe@users.noreply.github.com>
2020-07-11 16:27:01 -0300

    Update app.js

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: 'https://blooming-fortress-90318.herokuapp.com',
+    origin: 'https://bootcamp-grades-app.herokuapp.com/',
   })
 );
 
