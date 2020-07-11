bd923d122d1d2c66c16e3bdbf1b4d8fc5feb4f3b
erickmunizthe <34745038+erickmunizthe@users.noreply.github.com>
2020-07-11 16:28:36 -0300

    Update app.js

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: 'https://bootcamp-grades-app.herokuapp.com/',
+    origin: 'https://bootcamp-grades-app.herokuapp.com',
   })
 );
 
